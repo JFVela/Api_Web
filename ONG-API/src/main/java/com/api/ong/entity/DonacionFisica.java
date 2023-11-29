5bb7be241188e39b677bed7837b7030f97508fa6
DiegoBauti <diego.bautistamlp@gmail.com>
2023-11-28 21:46:36 -0500

    xde

--- ONG-API/src/main/java/com/api/ong/entity/DonacionFisica.java
+++ ONG-API/src/main/java/com/api/ong/entity/DonacionFisica.java
@@ -13,10 +13,6 @@ public class DonacionFisica {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id_fisica")
 	private Integer id_fisica;
-	@Column(name="donantes_dni")
-	private Integer donantes_dni;
-	@Column(name="local_donacion_id_local")
-	private Integer local_donacion_id_local;
 	@Column(name="descripcion")
 	private String descripcion;
 	@Column(name="estado")
